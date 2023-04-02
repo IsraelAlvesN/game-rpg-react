e4db017f833e39ab67af1d804a2501794ea485e1
Israel Alves <devisrael98@gmail.com>
2023-04-02 14:50:14 -0300

    Adicionada a movimentacao do personagem

--- src/components/Character/index.tsx
+++ src/components/Character/index.tsx
@@ -1,8 +1,19 @@
 import * as C from './styles'
 
-export const Character = () => {
+type Props = {
+    x: number;
+    y: number;
+}
+
+export const Character = ({x, y}: Props) => {
+    const size = 30;
+
     return(
-        <C.Container>
+        <C.Container
+            size={size}
+            left={x * size}
+            top={y * size}
+        >
             
         </C.Container>
     )
